fcc2e43a7326aebc20bcdb7e660625a62207467f
Nhan Nguyen <nhanthanhnguyen94@gmail.com>
2018-03-15 14:35:56 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ from keras.callbacks import Callback
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 
 
